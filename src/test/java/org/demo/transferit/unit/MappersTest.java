4d76f1be2b664c34862fbaf16a30731e531c806a
LeoMN <leomn138@gmail.com>
2020-01-27 06:30:50 +0100

    tests for mappers

--- src/test/java/org/demo/transferit/unit/MappersTest.java
+++ src/test/java/org/demo/transferit/unit/MappersTest.java
@@ -1,38 +1,68 @@
 package org.demo.transferit.unit;
 
 import io.quarkus.test.junit.QuarkusTest;
-import org.demo.transferit.utils.CurrencyChecker;
+import org.demo.transferit.dto.AccountSummary;
+import org.demo.transferit.dto.TransferDetails;
+import org.demo.transferit.dto.UserSummary;
+import org.demo.transferit.enums.Operation;
+import org.demo.transferit.mapper.AccountMapper;
+import org.demo.transferit.mapper.TransferMapper;
+import org.demo.transferit.mapper.UserProfileMapper;
+import org.demo.transferit.model.*;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-import java.util.List;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 
 @QuarkusTest
 public class MappersTest {
 
     @Test
-    public void isCurrency_returnsTrue_whenCurrencyIsValid() {
-        List<String> validCurrencies = Arrays.asList(
-                "JPY", "CNY", "SDG", "RON", "MKD", "MXN", "CAD", "USD",
-                "ZAR", "AUD", "NOK", "ILS", "ISK", "SYP", "LYD", "UYU", "YER", "CSD",
-                "EEK", "THB", "IDR", "LBP", "AED", "BOB", "QAR", "BHD", "HNL", "HRK",
-                "COP", "ALL", "DKK", "MYR", "SEK", "RSD", "BGN", "DOP", "KRW", "LVL",
-                "VEF", "CZK", "TND", "KWD", "VND", "JOD", "NZD", "PAB", "CLP", "PEN",
-                "GBP", "DZD", "CHF", "RUB", "UAH", "ARS", "SAR", "EGP", "INR", "PYG",
-                "TWD", "TRY", "BAM", "OMR", "SGD", "MAD", "BYR", "NIO", "HKD", "LTL",
-                "SKK", "GTQ", "BRL", "EUR", "HUF", "IQD", "CRC", "PHP", "SVC", "PLN"
-        );
+    public void toUserSummary_mapsUserProfileCorrectly() {
+        UserProfile userProfile = new UserProfile(UUID.randomUUID(), "email@example.com", "John", "Doe");
 
-        validCurrencies.forEach(currency -> assertTrue(CurrencyChecker.isCurrency(currency)));
+        UserSummary result = UserProfileMapper.toUserSummary(userProfile);
+
+        assertEquals(result.getEmail(), userProfile.getEmail());
+        assertEquals(result.getUuid(), userProfile.getUuid().toString());
+        assertEquals(result.getFirstName(), userProfile.getFirstName());
+        assertEquals(result.getLastName(), userProfile.getLastName());
     }
 
     @Test
-    public void isCurrency_returnsFalse_whenCurrencyIsNotValid() {
-        List<String> invalidCurrencies = Arrays.asList(null, "", "    ", "1234", "DOESNOTEXIST");
+    public void toAccountSummary_mapsAccountBalanceCorrectly() {
+        UserProfile userProfile = new UserProfile(UUID.randomUUID(), "email@example.com", "John", "Doe");
+        Account account = new Account(UUID.randomUUID(), userProfile, "safety", "EUR");
+        AccountBalance balance = new AccountBalance(account, BigDecimal.TEN);
 
-        invalidCurrencies.forEach(currency -> assertFalse(CurrencyChecker.isCurrency(currency)));
+        AccountSummary result = AccountMapper.toAccountSummary(balance, new ArrayList<>());
+
+        assertEquals(result.getUserUUID(), userProfile.getUuid());
+        assertEquals(result.getUuid(), account.getUuid());
+        assertEquals(result.getCurrency(), account.getCurrency());
+        assertEquals(result.getNickname(), account.getNickname());
+        assertTrue(result.getTransfers().isEmpty());
+    }
+
+    @Test
+    public void toTransferDetails_mapsEntryCorrectly() {
+        UserProfile userProfile = new UserProfile(UUID.randomUUID(), "email@example.com", "John", "Doe");
+        Account account = new Account(UUID.randomUUID(), userProfile, "safety", "EUR");
+        AccountingTransaction transaction = new AccountingTransaction(UUID.randomUUID(), 100000000, "transactionReference", "description");
+        AccountingEntry entry = new AccountingEntry(UUID.randomUUID(), "fromAccountReference", account, transaction, Operation.DEPOSIT, "EUR", BigDecimal.TEN);
+
+        TransferDetails result = TransferMapper.toTransferDetails(entry);
+
+        assertEquals(result.getSource(), entry.getFromAccountReference());
+        assertEquals(result.getCurrency(), entry.getCurrency());
+        assertEquals(result.getOperation(), entry.getOperation().toString());
+        assertEquals(result.getReference(), transaction.getReference());
+        assertEquals(result.getAmount(), entry.getAmount());
+        assertEquals(result.getDescription(), transaction.getDescription());
     }
 }
